359d7c8284ef3668c5342367313cf0734bc7c580
Okunnumuhammad <okunnumuhammad@gmail.com>
2023-02-17 17:25:59 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,15 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n < 100; n++)
+	{
 
-	putchar(n + '48');
-	if (n != 57)
+	putchar(n + '0');
+	if (n < 99)
 	{
 		putchar(',');
 		putchar(' ');
+	}
 	}
 	putchar('\n');
 
